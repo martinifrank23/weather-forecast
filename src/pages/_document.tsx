508b1e88129075681ac2ee77567e9d7e5d4c28b5
martinifrank23 <martinifrank23@gmail.com>
2023-02-20 14:25:31 -0500

    remove external css

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,12 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
-        />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
